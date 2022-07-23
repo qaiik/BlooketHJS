4081329933f446589110a519f828b864b5639f98
qaiik <82181231+qaiik@users.noreply.github.com>
2022-07-23 15:58:20 -0600

    Update blooket.js

--- blooket.js
+++ blooket.js
@@ -1,4 +1,3 @@
-// in progress - blooket hacking library
 let kit = {};
 
 kit._$krh = function _$kitreacthandler() {
@@ -14,3 +13,28 @@ unalloc("_$krh");
 
 kit.mode = () => location.pathname.split("/").at(-1);
 
+kit.setAmount = (type, value) => {
+    let values = {};
+    [type, type + "2"].forEach(m => values[m] = value);
+    kit.hack().stateNode.setState(values);
+    kit.hack().alternate.stateNode.setState(values);
+    return values
+}
+
+Object.defineProperty(kit, "ctype", {
+    get() {
+        return Object.entries(kit.hack().stateNode.state).map(a=>"number"==typeof a[1]?a[0]:null).filter(a=>Object.entries(kit.hack().stateNode.state).map(a=>"number"==typeof a[1]?a[0]:null).includes(a)&&Object.entries(kit.hack().stateNode.state).map(a=>"number"==typeof a[1]?a[0]:null).includes(a+"2"))[0]
+    },
+    set() {
+        return undefined
+    }
+});
+
+kit = {
+    ...kit,
+    data: {
+        get: function(v) {
+            return kit.hack()[a]?kit.hack()[a]:kit.hack().stateNode[a]?kit.hack().state[a]:kit.hack().stateNode.state[a]?kit.hack().stateNode.state[a]:kit.hack().memoizedProps[a]?kit.hack().memoizedProps[a]:kit.hack().stateNode.props[a]?kit.hack().stateNode.props[a]:void 0;
+        },
+    }
+}
